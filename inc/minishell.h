0740a700c5d0bd1a82d1857bd86e260d034cd392
msloot <msloot@student.42.fr>
2024-09-17 17:55:59 +0200

    added `libft` in header

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,12 +6,14 @@
 /*   By: msloot <msloot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 17:18:41 by msloot            #+#    #+#             */
-/*   Updated: 2024/09/17 17:19:42 by msloot           ###   ########.fr       */
+/*   Updated: 2024/09/17 17:55:29 by msloot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
+
+# include "../libft/inc/libft.h"
 
 # include <unistd.h>
 # include <stdbool.h>
