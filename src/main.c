6590ff4f683b549b0daba81888b10fcc1a24b37f
msloot <msloot@student.42.fr>
2024-09-25 19:44:26 +0200

    organize

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: msloot <msloot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 13:45:45 by gbonis            #+#    #+#             */
-/*   Updated: 2024/09/25 15:58:52 by msloot           ###   ########.fr       */
+/*   Updated: 2024/09/25 19:43:25 by msloot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,8 @@
 int	main(void)
 {
 	char	*prompt;
-	char	*cmd_str;
 
-	prompt = "minishell$ ";
-	cmd_str = readline(prompt);
-	parse(cmd_str);
+	prompt = readline("minishell$ ");
+	parse(prompt);
 	return (0);
 }
