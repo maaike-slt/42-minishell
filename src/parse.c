9e8dcc5a08517cd86c35a6511f397ba844057ea3
Guillaume Bonis <gbonis@paul-f5Br3s6.clusters.42paris.fr>
2024-10-06 15:40:20 +0200

    remove refactor comments

--- src/parse.c
+++ src/parse.c
@@ -43,57 +43,16 @@ bool	get_struct_values(t_values *values)
 	}
 	if (abs_path_in_values(values, split_str) == false)
 		return (false);
-//	values->abs_path = get_abs_path(*split_str);
-//	if (!values->abs_path)
-//	{
-//		ft_free_2d((void ***)&split_str, ft_2d_size((const void **)split_str));
-//		free(values->cmd_str);
-//		return (false);
-//	}
-//	if (values->abs_path)
-//	{
-//		if (!ft_strchr(values->cmd_str, '/'))			// this is only to free append (so "ls" or "cron", which become "/bin/ls"), otherwise abs_path is in fact line (because if line is "./a.out" and is viable, execve can use this)
-//			free(values->abs_path);
-//	}
 	ft_free_2d((void ***)&split_str, ft_2d_size((const void **)split_str));
 	free(values->cmd_str);
 	return (true);
 }
-	
-
 
 bool	parse(t_values *values)
 {
-//	char	*abs_path;
-//	char	**split_s;
 
 	if (get_struct_values(values) == false)
 		return (false);
-//	split_s = ft_split_whitespace(cmd_str);
-//	if (!split_s || !split_s[0])		// otherwise segfault if cmd_str is only spaces
-//	{
-//		free(cmd_str);
-//		if (split_s)
-//			free(split_s);
-//		return (false);
-//	}
-//	abs_path = get_abs_path(*split_s);
-//	if (!abs_path)
-//	{
-//		ft_free_2d((void ***)&split_s, ft_2d_size((const void **)split_s));
-//		free(cmd_str);
-//		return (false);
-//	}
-//						// perhaps before just have a get_struct_values() ?
-////	execute(values);	// put execve and getting $? in there
-//	//get_args()
-//	//execve(abs_path, args, environ);			// don't forget NULL terminate args
-//	if (abs_path)
-//	{
-//		if (!ft_strchr(cmd_str, '/'))			// this is only to free append (so "ls" or "cron", which become "/bin/ls"), otherwise abs_path is in fact line (because if line is "./a.out" and is viable, execve can use this)
-//			free(abs_path);
-//	}
-//	ft_free_2d((void ***)&split_s, ft_2d_size((const void **)split_s));
-//	free(cmd_str);
+	//execute(values);	
 	return (true);
 }
