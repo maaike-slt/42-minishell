07f9cbaff6f1c6cea6991039f1202afcc61d7fe1
Guillaume Bonis <gbonis@paul-f5Br5s7.clusters.42paris.fr>
2024-10-08 19:44:08 +0200

    started working on . or . . or . /bin/ls special case

--- src/path_splitting.c
+++ src/path_splitting.c
@@ -35,7 +35,12 @@ char	*get_abs_path(t_values *values, char *executable)
 	char	**paths;
 	char	*dir;
 
-	paths = split_env_path();					// no protection here, because still need to go to search dir func, protection is there
+	if (!ft_strcmp(".", executable))
+	{
+//		case_only_point(values, 		// but the problem here is that i dont really have the data to do this, i need to know the args, because . . or . / is diff than . /bin/ls, bash seems to care only for the second arg, i can parse manually here i guess
+		return (NULL);
+	}
+	paths = split_env_path();					// no protection here, because still need to go to search dir func, protection is there	// KEEP THIS RIGHT UNTIL EVAL, I WILL FORGET WHY I DID THIS OTHERWISE
 	dir = search_for_dir(values, paths, executable);
 	if (!dir)
 	{
