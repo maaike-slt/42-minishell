f5db21cf1ae88f926fd578162c3e501728c1d549
maaike-slt <maaike.sloot.26@gmail.com>
2024-12-01 20:03:27 +0100

    compiles

--- src/signals.c
+++ src/signals.c
@@ -6,14 +6,15 @@
 /*   By: msloot <msloot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/01 16:25:34 by msloot            #+#    #+#             */
-/*   Updated: 2024/12/01 16:31:06 by msloot           ###   ########.fr       */
+/*   Updated: 2024/12/01 20:01:49 by msloot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	ft_sigint(void)
+static void	ft_sigint(int num)
 {
+	(void)num;
 	{
 		ft_putstr_fd("\n", 1);
 		rl_replace_line("", 0);
