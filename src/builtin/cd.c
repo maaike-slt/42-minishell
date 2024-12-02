67d089282726bd98d342b37c72bfda0a0b5b2fb3
msloot <msloot@student.42.fr>
2024-12-02 22:27:06 +0100

    what the fork

--- src/builtin/cd.c
+++ src/builtin/cd.c
@@ -6,7 +6,7 @@
 /*   By: msloot <msloot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 17:42:45 by msloot            #+#    #+#             */
-/*   Updated: 2024/12/02 21:38:40 by msloot           ###   ########.fr       */
+/*   Updated: 2024/12/02 22:15:31 by msloot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	cd(t_args *arg)
 		// direct current position to HOME
 	ret = chdir(arg->argv[1]);
 	if (ret == -1)
-		return (perror(arg->argv[0]), D_ERROR);
-	return (D_OKAY);
+		return (perror(arg->argv[0]), EX_ERR);
+	return (EX_OK);
 	// update pwd to the new location
 }
--- src/dispatch.c
+++ src/dispatch.c
@@ -6,11 +6,34 @@
 /*   By: msloot <msloot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 19:23:35 by msloot            #+#    #+#             */
-/*   Updated: 2024/12/02 22:02:31 by msloot           ###   ########.fr       */
+/*   Updated: 2024/12/02 22:26:27 by msloot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+// tmp
+static int	i_fork(void)
+{
+	pid_t	pid;
+
+	pid = fork();
+	if (pid == -1)
+		return (perror("fork"), EX_ERR);
+	if (pid == 0)
+	{
+		ft_putstr("child\n");
+		sleep(1);
+		exit(0);	// FIXME: do not just exit(0)
+	}
+	else	// TODO: remove else branch if child quits
+	{
+		ft_putstr("parent\n");
+		waitpid(pid, NULL, 0);
+		ft_putstr("child is done, too much child labor\n");
+	}
+	return (EX_OK);
+}
 
 t_dispatch	dispatch(t_args *arg)
 {
@@ -20,9 +43,13 @@ t_dispatch	dispatch(t_args *arg)
 		return (D_EXIT);
 	}
 	else
-	{
 		if (!builtin(arg))
 			return (D_ERROR);
-	}
+	// else if  in path
+	// for testing
+	else if (true)
+		i_fork();
+	else
+		ft_putstr("command not found"); // TODO: finish this branch
 	return (D_OKAY);
 }
