83bb99d18969b2e20ed250bf2b51876ccbd6d2ae
adelille <adelille@student.42.fr>
2024-12-08 16:47:05 +0100

    protect malloc

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: msloot <msloot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/30 22:18:52 by msloot            #+#    #+#             */
-/*   Updated: 2024/12/08 16:46:30 by adelille         ###   ########.fr       */
+/*   Updated: 2024/12/08 16:46:53 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ int	main(int argc, char **argv, char **envp)
 	char	**envp_cpy;
 
 	envp_cpy = envdup(envp);
+	if (!envp_cpy)
+		return (1);
 	(void)argc;
 	(void)argv;
 	ft_putstr("Hello, World!\n");
