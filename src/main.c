6c80ee168edb14e9c940302fb24db763577e6528
adelille <adelille@student.42.fr>
2024-12-08 16:47:20 +0100

    order

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: msloot <msloot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/30 22:18:52 by msloot            #+#    #+#             */
-/*   Updated: 2024/12/08 16:46:53 by adelille         ###   ########.fr       */
+/*   Updated: 2024/12/08 16:47:15 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@ int	main(int argc, char **argv, char **envp)
 {
 	char	**envp_cpy;
 
+	ft_putstr("Hello, World!\n");
 	envp_cpy = envdup(envp);
 	if (!envp_cpy)
 		return (1);
 	(void)argc;
 	(void)argv;
-	ft_putstr("Hello, World!\n");
 	init_signals();
 	loop(envp_cpy);
 	rl_clear_history();
