25b0a7db04381c5811d9ac4ba542a1f9bfedd597
adelille <adelille@student.42.fr>
2024-12-08 16:46:49 +0100

    send the copy

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: msloot <msloot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/30 22:18:52 by msloot            #+#    #+#             */
-/*   Updated: 2024/12/08 16:45:38 by adelille         ###   ########.fr       */
+/*   Updated: 2024/12/08 16:46:30 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	main(int argc, char **argv, char **envp)
 	(void)argv;
 	ft_putstr("Hello, World!\n");
 	init_signals();
-	loop(envp);
+	loop(envp_cpy);
 	rl_clear_history();
 	return (0);
 }
