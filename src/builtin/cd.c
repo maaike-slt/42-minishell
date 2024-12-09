e3e4de274d9b88250d0c5721d7ab63ac0ec5ab57
adelille <adelille@student.42.fr>
2024-12-09 22:29:14 +0100

    deadcode

--- src/builtin/cd.c
+++ src/builtin/cd.c
@@ -6,7 +6,7 @@
 /*   By: msloot <msloot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 17:42:45 by msloot            #+#    #+#             */
-/*   Updated: 2024/12/09 22:25:55 by adelille         ###   ########.fr       */
+/*   Updated: 2024/12/09 22:28:58 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,8 @@ static int	go(const char *path)
 
 int	cd(int argc, char **argv, char **envp)
 {
-	int	ret;
-
 	dbg_builtin(argc, argv);
 	(void)envp;
-	ret = 0;
 	if (argc > 2)
 		return (error(argv[0], "too many arguments"), EX_ERR);
 	if (argc == 2)
