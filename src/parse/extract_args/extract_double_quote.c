9840199c6a1823710ac8aeb9106ed9d5143ab593
adelille <adelille@student.42.fr>
2024-12-09 21:27:04 +0100

    trailing whitespace

--- src/parse/extract_args/extract_double_quote.c
+++ src/parse/extract_args/extract_double_quote.c
@@ -6,7 +6,7 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/07 19:04:11 by adelille          #+#    #+#             */
-/*   Updated: 2024/12/08 19:00:48 by adelille         ###   ########.fr       */
+/*   Updated: 2024/12/09 21:26:40 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,14 +24,14 @@ char	*extract_double_quote(const char *line, size_t *i, char **envp)
 		if (line[*i] == '\\')
 		{
 			(*i)++;
-			ft_strpush(&ret, line[*i]); 
+			ft_strpush(&ret, line[*i]);
 		}
 		/*
 		else if (line[*i] == '$')
 			ret = ft_strjoin_free(ret, extract_variable(line, i, envp), true, true);
 		*/
 		else
-			ft_strpush(&ret, line[*i]); 
+			ft_strpush(&ret, line[*i]);
 		(*i)++;
 	}
 	return (ret);
