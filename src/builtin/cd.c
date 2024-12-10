45491eeed49c437834f42f0886e7c623084e3207
adelille <adelille@student.42.fr>
2024-12-10 19:36:23 +0100

    simplify

--- src/builtin/cd.c
+++ src/builtin/cd.c
@@ -6,7 +6,7 @@
 /*   By: msloot <msloot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 17:42:45 by msloot            #+#    #+#             */
-/*   Updated: 2024/12/10 19:27:20 by msloot           ###   ########.fr       */
+/*   Updated: 2024/12/10 19:36:08 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,18 +27,13 @@ int	cd(int argc, char **argv, char **envp)
 	char	*home_path;
 
 	dbg_builtin(argc, argv);
-	(void)envp;
 	if (argc > 2)
 		return (error(argv[0], "too many arguments"), EX_ERR);
 	if (argc == 2)
 		return (go(argv[1]));
-	if (argc == 1)
-	{
-		home_path = envp[find_env(envp, "HOME")];
-		if (!home_path)
-			return (ft_puterr("HOME not set"), EX_ERR);
-		return (go(home_path));
-	}
-	return (go(argv[1]));
+	home_path = envp[find_env(envp, "HOME")];
+	if (!home_path)
+		return (ft_puterr("HOME not set"), EX_ERR);
+	return (go(home_path));
 	// update pwd to the new location
 }
