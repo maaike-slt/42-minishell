e40c2444663dcdb5544e1495c62971fd1ca24095
adelille <adelille@student.42.fr>
2024-12-11 00:08:46 +0100

    had an idea

--- src/main.c
+++ src/main.c
@@ -6,13 +6,33 @@
 /*   By: msloot <msloot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/30 22:18:52 by msloot            #+#    #+#             */
-/*   Updated: 2024/12/09 21:30:58 by adelille         ###   ########.fr       */
+/*   Updated: 2024/12/11 00:06:19 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 #ifndef TEST
+
+static void	rl_clean(void)
+{
+	rl_clear_history();
+	rl_clear_signals();
+	rl_cleanup_after_signal();
+	rl_free_line_state();
+	rl_callback_sigcleanup();
+	rl_callback_handler_remove();
+	rl_free_undo_list();
+	rl_free_keymap(rl_get_keymap());
+	rl_free_keymap(rl_get_keymap_by_name("emacs"));
+	rl_free_keymap(rl_get_keymap_by_name("emacs-standard"));
+	rl_free_keymap(rl_get_keymap_by_name("emacs-meta"));
+	rl_free_keymap(rl_get_keymap_by_name("emacs-ctlx"));
+	rl_free_keymap(rl_get_keymap_by_name("vi"));
+	rl_free_keymap(rl_get_keymap_by_name("vi-move"));
+	rl_free_keymap(rl_get_keymap_by_name("vi-command"));
+	rl_free_keymap(rl_get_keymap_by_name("vi-insert"));
+}
 
 int	main(int argc, char **argv, char **envp)
 {
@@ -26,7 +46,8 @@ int	main(int argc, char **argv, char **envp)
 	(void)argv;
 	init_signals();
 	loop(envp_cpy);
-	rl_clear_history();
+	ft_2d_free((void ***)&envp_cpy, ft_2d_size((const void **)envp_cpy));
+	rl_clean();
 	return (0);
 }
 
