2b0c77a02737243a59dc504d1785ab1e3957d9e4
adelille <adelille@student.42.fr>
2025-02-16 13:50:15 +0100

    working file redirection without heredoc

--- src/redirection/create_file_redirection.c
+++ src/redirection/create_file_redirection.c
@@ -6,7 +6,7 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/09 17:11:37 by adelille          #+#    #+#             */
-/*   Updated: 2025/02/16 13:13:51 by adelille         ###   ########.fr       */
+/*   Updated: 2025/02/16 13:49:07 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,51 @@ static void	shift_argv(t_exp *exp, size_t i)
 	exp->argc -= 1;
 }
 
+static bool	close_existing(t_exp *exp, char ir)
+{
+	if ((ir == IR_FILE_OUT || ir == IR_FILE_OUT_APPEND)
+		&& exp->outfd > STDERR_FILENO)
+	{
+		if (close(exp->outfd) == -1)
+			return (perror("close"), false);
+	}
+	else if ((ir == IR_FILE_IN || ir == IR_HEREDOC)
+		&& exp->infd > STDERR_FILENO)
+	{
+		if (close(exp->infd) == -1)
+			return (perror("close"), false);
+	}
+	return (true);
+}
+
+static bool	handle_redirection(t_exp *exp, size_t i)
+{
+	char	ir;
+
+	ir = exp->argv[i][0];
+	if (!close_existing(exp, ir))
+		return (false);
+	if (ir == IR_FILE_OUT)
+		exp->outfd = open(exp->argv[i + 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	else if (ir == IR_FILE_OUT_APPEND)
+		exp->outfd = open(exp->argv[i + 1],
+				O_WRONLY | O_CREAT | O_APPEND, 0644);
+	else if (ir == IR_FILE_IN)
+		exp->infd = open(exp->argv[i + 1], O_RDONLY);
+	else if (ir == IR_HEREDOC)
+	{
+		dbg("heredoc not implemented yet");
+		exit(42);
+	}
+	if (exp->outfd == -1 || exp->infd == -1)
+		return (perror("open"), false);
+	free(exp->argv[i]);
+	free(exp->argv[i + 1]);
+	shift_argv(exp, i);
+	shift_argv(exp, i);
+	return (true);
+}
+
 static bool	create_single_exp_file_redirection(t_exp *exp)
 {
 	size_t	i;
@@ -29,15 +74,13 @@ static bool	create_single_exp_file_redirection(t_exp *exp)
 	i = 0;
 	while (i < (size_t)exp->argc)
 	{
-		if (exp->argv[i][0] == IR_FILE_OUT)
+		if (exp->argv[i][0] == IR_FILE_OUT
+			|| exp->argv[i][0] == IR_FILE_OUT_APPEND
+			|| exp->argv[i][0] == IR_FILE_IN
+			|| exp->argv[i][0] == IR_HEREDOC)
 		{
-			if (exp->outfd > STDERR_FILENO)
-				close(exp->outfd);
-			exp->outfd = open(exp->argv[i + 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-			free(exp->argv[i]);
-			free(exp->argv[i + 1]);
-			shift_argv(exp, i);
-			shift_argv(exp, i);
+			if (!handle_redirection(exp, i))
+				return (false);
 		}
 		else
 			i++;
