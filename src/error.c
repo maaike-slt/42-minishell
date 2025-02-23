a1f00c2957f55e2332fa776fae84038afe031132
adelille <adelille@student.42.fr>
2025-02-23 22:33:25 +0100

    no new line before printing error

--- src/error.c
+++ src/error.c
@@ -6,7 +6,7 @@
 /*   By: msloot <msloot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 13:26:40 by msloot            #+#    #+#             */
-/*   Updated: 2025/02/16 16:18:42 by adelille         ###   ########.fr       */
+/*   Updated: 2025/02/23 22:33:08 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 void	error(const char *err_src, const char *msg)
 {
-	ft_putchar_fd('\n', STDERR_FILENO);
 	ft_putstr_fd(Y_B_RED, STDERR_FILENO);
 	ft_putstr_fd("minishell", STDERR_FILENO);
 	ft_putstr_fd(Y_RESET, STDERR_FILENO);
